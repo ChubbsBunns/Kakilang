e34281d2c64d2f55395c9003a81e060a3dd0049a
Dylan Ho <troskproductions@gmail.com>
2022-05-26 16:29:15 +0800

    worked on landing page

--- kakilang-web-app/src/App.js
+++ kakilang-web-app/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-/*
+/* WILLIAM INITIAL STUFF
 import { Routes, BrowserRouter, Route } from "react-router-dom";
 
 //component imports
@@ -10,7 +10,7 @@ import Registration from "./components/Registration.component";
 */
 function App() {
   return (
-    /*
+    /* WILLIAM INITIAL STUFF
     <div className="App">
       <header className="App-header">
         <a href="/" className="App-link">
@@ -29,14 +29,11 @@ function App() {
       </div>
     </div>
     */
-   <body>
-    <div className="header-main">
-      <div className="entire-sidebar">
-
-
+    <body>
+      <div className="header-main">
         <div className="sidebar">
           <header>My Kakis!</header>
-          <ul>
+          <ul className="sidebar-menu">
             <li>
               <a href="#">
                 <span className="fa fa-house"></span>
@@ -49,28 +46,39 @@ function App() {
                 <span id="Floor"> Floormates</span>
               </a>
             </li>
-            <li>
-              <a href="#">
-                <span className="fa fa-baseball-bat-ball"></span>
-                <span id="Interest_Groups"> CCAs/IGs</span>
-              </a>
+            <li className="sub-menu">
+              <a href="#"><i className="fa fa-baseball-bat-ball"></i> CCAs/IGs<i className="fa fa-chevron-circle-down indicator"></i></a>
+              <ul>
+                <li>
+                  <a href="#"><i className="fa fa-bicycle"></i>Cycling</a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i className="fa fa-volleyball"></i>Volleyball
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i className="fa fa-masks-theater"></i>USProductions
+                  </a>
+                </li>
+              </ul>
             </li>
           </ul>
         </div>
-      </div>
 
-      <div className="title" id="banner-text">
-        Kakilang!
-        <div className="banner">
-          <div className="UI" id="list_of_people">
-            Peoplepeoplepeople
-          </div>
-          <div className="UI" id="text_interface">
-            textingtextingtexting
+        <div className="title" id="banner-text">
+          Kakilang!
+          <div className="banner">
+            <div className="UI" id="list_of_people">
+              Peoplepeoplepeople
+            </div>
+            <div className="UI" id="text_interface">
+              textingtextingtexting
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </body>
   );
 }
